23cb409aa1d9812e9939441d12384f34520232c4
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-10 22:29:13 +0000

    Add import bootstrap to Index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
