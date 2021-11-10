027a17b890174c3f20d64763bfb9ca2853f24cf8
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-10 22:26:35 +0000

    Add Router and main to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
-import { Home } from './Components/Home';
+import "./App.css";
+import { Home } from "./Components/Home";
 
 const Routes = () => {
-  return(
-  <Switch>
-    <Route path={"/"} component={Home}/>
-  </Switch>
-  )
-}
+  return (
+    <Switch>
+      <Route path={"/"} component={Home} />
+    </Switch>
+  );
+};
 
 export const App = () => {
   return (
-    <Routes/>
-  )
-}
+    <Router>
+      <main>
+        <Routes />
+      </main>
+    </Router>
+  );
+};
