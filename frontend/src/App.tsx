8b8a3f0cd0fd1a9b2b640523f8ba7285b1df0f01
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-10 22:24:19 +0000

    Add Routes and fix react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import { Home } from './Components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Routes = () => {
+  return(
+  <Switch>
+    <Route path={"/"} component={Home}/>
+  </Switch>
+  )
 }
 
-export default App;
+export const App = () => {
+  return (
+    <Routes/>
+  )
+}
