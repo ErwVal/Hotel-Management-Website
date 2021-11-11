4d13c84096c76276085fb920388881072c6840d8
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-11 01:08:53 +0000

    Add Header component to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/Home";
+import { Header } from "./components/Header";
 
 const Routes: React.FunctionComponent = () => {
   return (
@@ -15,6 +16,7 @@ export const App: React.FunctionComponent = () => {
   return (
     <Router>
       <main>
+        <Header/>
         <Routes />
       </main>
     </Router>
--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -3,14 +3,14 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export const Header: React.FunctionComponent = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+      <Nav className="ms-auto">
+        <Nav.Link href="/signup">Sign Up</Nav.Link>
+        <Nav.Link href="/login">Login</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
