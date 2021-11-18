c1fc382d45662c048d792852f8f74b49a44a6c71
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-18 16:06:15 +0000

    backend test branch working

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace react_typescript_dotnet_app
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/build";
+                configuration.RootPath = "frontend/build";
             });
         }
 
@@ -59,7 +59,7 @@ namespace react_typescript_dotnet_app
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "frontend";
 
                 if (env.IsDevelopment())
                 {
