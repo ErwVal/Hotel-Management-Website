23c04676fb0cb1a8eb06df9489b8d485e303fa9b
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-18 17:51:15 +0000

    Add RoomsController

--- backend/Controllers/RoomsController.cs
+++ backend/Controllers/RoomsController.cs
@@ -1,2 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using react_typescript_dotnet_app.Repositories;
+using react_typescript_dotnet_app.Services;
 using react_typescript_dotnet_app.Database.Models;
+using System.Linq;
+using System.Collections.Generic;
 
+namespace react_typescript_dotnet_app.Controllers
+{
+    [ApiController]
+    [Route("/rooms")]
+
+    public class RoomsController : ControllerBase
+    {
+            private readonly ILogger<RoomsController> _logger;
+            private readonly IRoomsRepo _roomsRepo;
+            private readonly IRoomsService _roomsService;
+
+            public RoomsController(ILogger<RoomsController> logger, IRoomsRepo roomsRepo, IRoomsService roomsService)
+            {
+                _logger = logger;
+                _roomsRepo = roomsRepo;
+                _roomsService = roomsService;
+            }
+
+               ///<summary>
+    /// Gets the list of rooms from the database.
+    ///</summary>
+    [HttpGet]
+    public ActionResult<RoomsListResponse> GetRooms()
+    {
+      return new RoomsListResponse
+      {
+        Rooms = _roomsService
+          .GetRooms()
+          .Select(s => new RoomsResponse(s))
+          .ToList()
+      };
+    }
+
+    }
+
+}
