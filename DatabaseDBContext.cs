49e631101b58c7406a272315f3f98bf704e0c393
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-18 16:49:08 +0000

    Add DbSet tables to DBContext

--- DatabaseDBContext.cs
+++ DatabaseDBContext.cs
@@ -1,11 +1,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+using react_typescript_dotnet_app.Database.Models;
+using Npgsql;
 
 namespace react_typescript_dotnet_app
 {
     public class DatabaseDBContext : DbContext
     {
-        
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
     }
 }
