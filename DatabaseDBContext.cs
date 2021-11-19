35260ab675c5dc61a303f7bca68dba230ac65c05
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-19 17:14:10 +0000

    Add migration

--- DatabaseDBContext.cs
+++ DatabaseDBContext.cs
@@ -16,47 +16,7 @@ namespace react_typescript_dotnet_app
             DbContextOptionsBuilder optionsBuilder
         )
         {
-            optionsBuilder.UseNpgsql(GetConnectionString());
+            optionsBuilder.UseNpgsql("Host=localhost;Database=hotelDB;Username=postgres;Password=Xq4Y0f1fdoP6Hr");
         }
-
-        private static string GetConnectionString()
-        {
-            var databaseUrl =
-                Environment.GetEnvironmentVariable("DATABASE_URL");
-            if (databaseUrl == null)
-            {
-                throw new Exception("Environment variable 'DATABASE_URL' must not be null");
-            }
-
-            bool useSsl = true;
-            var useSslVariable = Environment.GetEnvironmentVariable("USE_SSL");
-            if (useSslVariable != null)
-            {
-                if (!Boolean.TryParse(useSslVariable, out useSsl))
-                {
-                    throw new Exception("Environment variable 'USE_SSL' must be parse-able as bool");
-                }
-            }
-
-            var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(':');
-
-            var builder =
-                new NpgsqlConnectionStringBuilder {
-                    Host = databaseUri.Host,
-                    Port = databaseUri.Port,
-                    Username = userInfo[0],
-                    Password = userInfo[1],
-                    Database = databaseUri.LocalPath.TrimStart('/')
-                };
-            if (useSsl)
-            {
-                builder.SslMode = SslMode.Require;
-                builder.TrustServerCertificate = true;
-            }
-
-            return builder.ToString();
-        }
-
     }
 }
---
+++ Migrations/20211119171316_CreatehotelDB.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace react_typescript_dotnet_app.Migrations
+{
+    public partial class CreatehotelDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HotelName = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    HashedPassword = table.Column<string>(type: "text", nullable: true),
+                    Salt = table.Column<byte[]>(type: "bytea", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CheckIn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CheckOut = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    GuestName = table.Column<string>(type: "text", nullable: true),
+                    NumGuests = table.Column<int>(type: "integer", nullable: false),
+                    HotelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomType = table.Column<int>(type: "integer", nullable: false),
+                    Images = table.Column<List<string>>(type: "text[]", nullable: true),
+                    RoomPrice = table.Column<double>(type: "double precision", nullable: false),
+                    MaxGuests = table.Column<int>(type: "integer", nullable: false),
+                    HotelId = table.Column<int>(type: "integer", nullable: true),
+                    ReservationId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_HotelId",
+                table: "Reservations",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_HotelId",
+                table: "Rooms",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_ReservationId",
+                table: "Rooms",
+                column: "ReservationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
