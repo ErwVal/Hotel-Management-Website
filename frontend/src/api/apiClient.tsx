cd2e9380338b330a4dd23eb874d3daaff7323120
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-24 23:30:17 +0000

    Add fetchRoomsByQuery to apiClient

--- frontend/src/api/apiClient.tsx
+++ frontend/src/api/apiClient.tsx
@@ -5,16 +5,56 @@ export interface NewBooking {
   numGuests: number;
 }
 
+enum roomType {
+  Single,
+  Double,
+  Twin,
+  Suite,
+}
+
+enum available {
+  No,
+  Yes,
+}
+export interface Room {
+  id: number;
+  roomType: roomType;
+  available: available;
+  images: string[];
+  roomPrice: number;
+  maxGuests: number;
+}
+export interface RoomsListResponse {
+  room: Room[];
+}
+
 const baseUrl = process.env["REACT_APP_BACKEND_DOMAIN"];
 
 export const createBooking = async (newBooking: NewBooking) => {
-  const response = await fetch(`${baseUrl}/booking/create`, {
+  const response = await fetch(`${baseUrl}/reservation/create`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newBooking),
   });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return await response.json();
+};
+
+export const fetchRoomsListByQuery = async (
+  location: string,
+      checkInDate: Date,
+      checkOutDate: Date,
+      numGuests: number
+): Promise<RoomsListResponse> => {
+  const response = await fetch(
+    `${baseUrl}/rooms/by-query?location=${location}&checkInDate=${checkInDate}&checkOutDate=${checkOutDate}&numGuests=${numGuests}`
+  );
 
   if (!response.ok) {
     throw new Error(response.statusText);
