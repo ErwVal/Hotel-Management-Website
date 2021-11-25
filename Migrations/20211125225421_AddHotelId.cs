41b6e74c35054c574eaebf57c2e907e550d6819e
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-11-25 22:55:11 +0000

    Add migration

---
+++ Migrations/20211125225421_AddHotelId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace react_typescript_dotnet_app.Migrations
+{
+    public partial class AddHotelId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Rooms",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
