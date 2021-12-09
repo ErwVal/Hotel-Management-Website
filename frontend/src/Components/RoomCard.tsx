9036c151d1a4fa708e9341c6a3b096d8d8bcee9a
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-12-09 11:11:58 +0000

    Add styling to RoomCard

--- frontend/src/Components/RoomCard.tsx
+++ frontend/src/Components/RoomCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { Room } from "../api/apiClient";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import { motion } from "framer-motion";
 
 interface Props {
   room: Room;
@@ -11,19 +13,51 @@ interface Props {
   checkOutDate: string;
 }
 
+enum roomType {
+  Single,
+  Double,
+  Twin,
+  Suite,
+}
+
+const cardVariants = {
+  hidden: {
+    opacity: 0,
+    x: "200vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 2,
+      when: "beforeChildren",
+    },
+  },
+};
+
 export const RoomCard: React.FunctionComponent<Props> = (
   props: Props
 ) => {
+  
+  let departure = moment(props.checkOutDate);
+  let arrival = moment(props.checkInDate);
+
+  let lengthOfStay = moment.duration(departure.diff(arrival)).asDays();
+
   return (
+    <motion.div variants={cardVariants} initial="hidden" animate="visible">
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={props.room.images[0]} />
       <Card.Body>
-        <Card.Title>Room Type {props.room.roomType}</Card.Title>
+        <Card.Title>{roomType[props.room.roomType]} room</Card.Title>
         <Card.Text>
-          Room Price {props.room.roomPrice}
+          {props.room.roomPrice} x {lengthOfStay} nights
+          <br/>
+         Total stay: {(props.room.roomPrice * lengthOfStay).toFixed(2)}
         </Card.Text>
         <Link to={`/rooms/${props.room.id}/${props.hotelId}/${props.numGuests}/${props.checkInDate}/${props.checkOutDate}`}>See room</Link>
       </Card.Body>
     </Card>
+    </motion.div>
   );
 };
