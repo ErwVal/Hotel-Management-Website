347282dcb84ba644bbcb026404bb60a0ca2f3d9b
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-12-09 22:25:02 +0000

    Add migration

---
+++ Migrations/20211209222425_AddHotelIdToReservationsDatabase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace react_typescript_dotnet_app.Migrations
+{
+    public partial class AddHotelIdToReservationsDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Hotels_HotelId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Reservations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Hotels_HotelId",
+                table: "Reservations",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Hotels_HotelId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Reservations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Hotels_HotelId",
+                table: "Reservations",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
