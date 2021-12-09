b39ec708c83eb172d20660a1f32e7d05279268c2
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-12-09 15:03:16 +0000

    Add styling to RoomBookingCard

--- frontend/src/Components/RoomBookingCard.tsx
+++ frontend/src/Components/RoomBookingCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Room } from "../api/apiClient";
 import { Container, Carousel, Row, Col, Button } from "react-bootstrap";
+import { FaWifi, FaSmokingBan, FaBed, FaPoundSign } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
+import moment from "moment";
 
 interface Props {
   room: Room;
@@ -11,64 +14,130 @@ interface Props {
 }
 
 enum roomType {
-    Single,
-    Double,
-    Twin,
-    Suite,
-  }
+  Single,
+  Double,
+  Twin,
+  Suite,
+}
 
 export const RoomBookingCard: React.FunctionComponent<Props> = (
   props: Props
 ) => {
+  let departure = moment(props.checkOutDate);
+  let arrival = moment(props.checkInDate);
+  let lengthOfStay = moment.duration(departure.diff(arrival)).asDays();
 
-    let location = "";
+  let location = "";
 
-    if(props.hotelId === 1)
-    {
-        location = "Cancun";
-    }
-    else if (props.hotelId === 2)
-    {
-        location = "Tulum";
-    }
-    else
-    {
-        location = "Playa del Carmen";
-    }
+  if (props.hotelId === 1) {
+    location = "Cancun";
+  } else if (props.hotelId === 2) {
+    location = "Tulum";
+  } else {
+    location = "Playa del Carmen";
+  }
   return (
-    <Container fluid>
-      <h2>{roomType[props.room.roomType]} room for stay in {location}</h2>
-      <Row>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-50"
-              src={props.room.images[0]}
-              alt="room"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={props.room.images[1]}
-              alt="room"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={props.room.images[2]}
-              alt="room"
-            />
-          </Carousel.Item>
-        </Carousel>
-      </Row>
-      <Row>
-        {" "}
-        <Col> Price per night: {props.room.roomPrice}. </Col>{" "}
-        <Col> Accommodates up to {props.room.maxGuests} guests.</Col>
-      </Row>
-      <Button>Book this room</Button>
+    <Container>
+      <div className="div-room-booking-card">
+        <Row>
+          <h3>
+            {roomType[props.room.roomType]} room in {location}
+          </h3>
+        </Row>
+        <Row>
+          <Col>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src={props.room.images[0]}
+                  alt="room"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src={props.room.images[1]}
+                  alt="room"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block"
+                  src={props.room.images[2]}
+                  alt="room"
+                />
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+          <Col>
+            <p>
+              The standard room is decorated in a minimalist style that blends
+              perfectly with the hotel’s theme and setting, designed in an earth
+              tone and furnished with wooden furniture. Room’s amenities are all
+              organic products, i.e. soap, shampoo, lotion, and toothbrush, for
+              the purpose of environmental protection. The room is equipped with
+              a queen-size bed, a digital TV, with a selection of channels on
+              Netflix provided for your entertainment, including warm shower, a
+              pantry, towels, shampoo and shower gel, a hair dryer, slippers and
+              bathrobes.
+            </p>
+          </Col>
+        </Row>
+
+        <br />
+        <br />
+        <Row>
+          <Col>
+            {" "}
+            <FaPoundSign className="icon-wifi icon" size={30} />
+            {props.room.roomPrice} p/night
+          </Col>{" "}
+          <Col>
+            {" "}
+            <FaWifi className="icon-wifi icon" size={30} /> Free Wifi
+          </Col>
+          <Col>
+            <BsFillPersonFill className="icon-bed icon" size={30} /> x
+            {props.room.maxGuests}
+          </Col>
+          <Col>
+            <FaBed className="icon-bed icon" size={30} /> x
+            {roomType[props.room.roomType] == "Twin" ? "2 beds" : "1 bed"}
+          </Col>
+          <Col>
+            <FaSmokingBan className="icon-bed icon" size={30} /> No-smoking
+          </Col>
+        </Row>
+
+        <br />
+        <br />
+
+        <Row>
+          <Col>
+            <p className="p-room-price">
+              <h4>Reservation details:</h4> <br />
+              <ul>
+                <li>
+                  {roomType[props.room.roomType]} room in {location}
+                </li>
+                <li>Adults x {props.numGuests}</li>
+                <li>
+                  {lengthOfStay} nights x £{props.room.roomPrice} p/night
+                </li>
+              </ul>
+              <h5>
+                  {" "}
+                  Total for stay: £
+                  {(props.room.roomPrice * lengthOfStay).toFixed(2)}
+                </h5>
+            </p>
+          </Col>
+          <Col>
+            <button>Reserve now</button>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
